bdbe4b501599697ebb072a03e841c33da4c3bf0a
manideep pullalachervu <pmanideepnaidu@gmail.com>
2022-04-22 23:19:59 +0530

    feat: Add db and oAuth

---
+++ index.js
@@ -0,0 +1,92 @@
+const path = require("path");
+
+require("dotenv").config();
+const cors = require("cors");
+
+const axios = require("axios");
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+
+const Team = require("./models/team.model");
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
+
+const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, KEY, PORT, MONGODB_URI } =
+  process.env;
+
+app.listen(PORT || 3000, () => {
+  try {
+    mongoose.connect(
+      MONGODB_URI,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      () => console.log("Mongoose is connected")
+    );
+  } catch (e) {
+    console.log("could not connect");
+  }
+  console.log(`listening on port ${PORT}`);
+});
+
+app.get("/callback", generateAccessToken, async (req, res) => {
+  try {
+    const slackData = req.slackData;
+    const {
+      team: { id: teamId, name },
+      access_token: accessToken,
+    } = slackData;
+
+    const oldTeam = await Team.find({ teamId }).lean();
+    if (!oldTeam.length) {
+      await Team.create({ teamId, name, accessToken });
+    } else {
+      await Team.findOneAndUpdate({ teamId }, { accessToken });
+    }
+
+    res.send("success");
+  } catch (err) {
+    res.status(500).send(`Something went wrong: ${err}`);
+  }
+});
+
+async function generateAccessToken(req, res, next) {
+  const code = req.query.code;
+  const url = "https://slack.com/api/oauth.v2.access";
+  const requestBody = new URLSearchParams(
+    Object.entries({
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: REDIRECT_URI,
+    })
+  ).toString();
+  const { data } = await postData(url, requestBody);
+  req.slackData = data;
+
+  next();
+}
+
+// Example POST method implementation:
+async function postData(url = "", data = "") {
+    // Default options are marked with *
+    const response = await axios({
+      url,
+      method: "post",
+      mode: "cors", // no-cors, *cors, same-origin
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      data, // body data type must match "Content-Type" header
+    });
+    return response; // parses JSON response into native JavaScript objects
+  }
